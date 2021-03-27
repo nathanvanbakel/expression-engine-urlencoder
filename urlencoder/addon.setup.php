c3ab036b3ef276e7f136b18503c2d8740a33b5fc
Nathan van Bakel <info@voorloper.com>
2021-03-27 12:13:38 +0100

    Update addon.setup.php
    Small changes: namespacing update, removing php closing tag.

--- urlencoder/addon.setup.php
+++ urlencoder/addon.setup.php
@@ -2,10 +2,10 @@
   
 return array(
       'author'      => 'Nathan van Bakel',
-      'author_url'  => 'https://voorloper.com',
+      'author_url'  => 'https://www.voorloper.com',
       'name'        => 'URL encode',
       'description' => 'Encodes a string for save transfer via URL',
-      'version'     => '1.0',
-      'namespace'   => 'Voorloper\urlencoder',
+      'version'     => '1.0.1',
+      'namespace'   => 'voorloper\urlencoder',
       'docs_url'    => 'https://github.com/nathanvanbakel/expression-engine-urlencoder'
 );
